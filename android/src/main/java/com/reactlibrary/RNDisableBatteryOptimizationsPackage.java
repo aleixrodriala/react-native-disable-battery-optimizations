eddb75718c24898f60b4b2f5ebfed27308edfd92
Aleix Rodríguez Alameda <aleixrodriala@gmail.com>
2018-12-11 01:24:18 +0100

    Typo

--- android/src/main/java/com/reactlibrary/RNDisableBatteryOptimizationsPackage.java
+++ android/src/main/java/com/reactlibrary/RNDisableBatteryOptimizationsPackage.java
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNDisableBatteryOptimizationsPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNPowerManagerModule(reactContext));
+      return Arrays.<NativeModule>asList(new RNDisableBatteryOptimizationsModule(reactContext));
     }
 
     // Deprecated from RN 0.47
